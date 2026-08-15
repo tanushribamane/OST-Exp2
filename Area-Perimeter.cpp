c4e4b4a6c785f867c7a2cdf29880e697caebf850
riddhikale <riddhiskale28@gmail.com>
2026-08-15 19:20:20 +0530

    implement area and perimeter calculations for rectangle and square

--- Area-Perimeter.cpp
+++ Area-Perimeter.cpp
@@ -11,4 +11,27 @@ int main()
     cout << "2. Square\n";
     cout << "Enter choice: ";
     cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Enter length and width: ";
+        cin >> a >> b;
+
+        cout << "Area = " << a * b << endl;
+        cout << "Perimeter = " << 2 * (a + b) << endl;
+    }
+    else if (choice == 2)
+    {
+        cout << "Enter side: ";
+        cin >> a;
+
+        cout << "Area = " << a * a << endl;
+        cout << "Perimeter = " << 4 * a << endl;
+    }
+    else
+    {
+        cout << "Invalid choice";
+    }
+
+    return 0;
 }
